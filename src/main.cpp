9bd6da34c6834ecec73689f12d976fbc4e787a8b
drac0nic <spenser.m.bray@gmail.com>
2014-01-16 11:07:47 -0600

    Added .gitignore and updated some things slightly.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <irrlicht.h>
+#include <iostream>
 
+using namespace std;
 using namespace irr;
 using namespace core;
 using namespace scene;
@@ -9,10 +11,58 @@ using namespace gui;
 
 int main(int argc, char* argv[])
 {
-  IrrlichtDevice* device = createDevice(video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16, false, false, false, 0);
+  char chbuffer;
+  IrrlichtDevice* device;
+  video::E_DRIVER_TYPE driverType;
   
-  if(!device) return 1;
+  // Initialize rendering device.
+  cout << "Please select the rendering device you would like to use:" << endl
+       << " (a) OpenGL 1.5" << endl
+       << " (b) Direct3D 9.0c" << endl
+       << " (c) Direct3D 8.1" << endl
+       << " (d) Burning Software Render" << endl
+       << " (e) Software Renderer" << endl
+       << " (f) Null Device" << endl
+       << " (x) Exit (or any other key)" << endl
+       << "\nChoice: ";
   
+  cin >> chbuffer;
+  
+  switch(chbuffer) {
+  case 'a':
+    driverType = video::EDT_OPENGL;
+    break;
+    
+  case 'b':
+    driverType = video::EDT_DIRECT3D9;
+    break;
+    
+  case 'c':
+    driverType = video::EDT_DIRECT3D8;
+    break;
+    
+  case 'd':
+    driverType = video::EDT_BURNINGSVIDEO;
+    break;
+    
+  case 'e':
+    driverType = video::EDT_SOFTWARE;
+    break;
+    
+  case 'f':
+    driverType = video::EDT_NULL;
+    break;
+    
+  default:
+    return 1;
+  }
+  
+  // Create device and exit if creation failed.
+  device = createDevice(driverType, core::dimension2d<u32>(640, 480));
+  
+  if(device == 0) return 1;
+  
+  // Setup device.
   device->setWindowCaption(L"IRRLICHT WINDOW");
   
   IVideoDriver* driver = device->getVideoDriver();
